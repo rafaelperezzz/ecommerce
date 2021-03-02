6517733831a929be13ee56c9e45157cd035641e6
rafael <rpeerez18@gmail.com>
2021-03-01 21:03:48 -0300

    Admin - Produtos

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
-use \Hcode\Page;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-	$page = new Page();
-
-	$page->SetTpl("index");
-
-});
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-products.php");
 
 $app->run();
 
- ?>
+?>s
