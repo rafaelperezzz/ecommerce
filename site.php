98e7220ca17abbc3e824f1b3e71f832947e5ac50
rafael <rpeerez18@gmail.com>
2021-05-11 00:40:58 -0300

    carinho calculo frete

--- site.php
+++ site.php
@@ -70,7 +70,8 @@ $app->get("/cart", function(){
 
 	$page->setTpl("cart", [
 		'cart'=>$cart->getValues(),
-		'products'=>$cart->getProducts()
+		'products'=>$cart->getProducts(),
+		'error'=>Cart::getMsgError()
 	]);
 });
 
@@ -85,6 +86,7 @@ $app->get("/cart/:idproduct/add", function($idproduct){
 	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
 
 	for ($i = 0; $i < $qtd; $i++){
+		
 		$cart->addProduct($product);
 	}
 
@@ -123,6 +125,16 @@ $app->get("/cart/:idproduct/remove", function($idproduct){
 	header("Location: /cart");
 	exit;
 	
+});
+
+$app->post("/cart/freight", function(){
+
+	$cart = Cart::getFromSession();
+
+	$cart->setFreight($_POST['zipcode']);
+
+	header("Location: /cart");
+	exit;
 });
 
 ?>
