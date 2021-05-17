0418cc4987e9339e7ca3ad6e4924d0c4466ffaa3
rafael <rpeerez18@gmail.com>
2021-05-17 09:43:09 -0300

    Site Login

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php 
+
+use \Hcode\Model\User;
+use \Hcode\Model\Cart;
+
+function formatPrice($vlprice){
+
+	return number_format($vlprice, 2, ",", ".");
+
+}
+
+function checkLogin($inadmin = true){
+
+	return User::checkLogin($inadmin);
+
+}
+
+function getUserName(){
+
+	$user = User::getFromSession();
+
+	return $user->getdesperson();
+
+}
+
+
+ ?>
